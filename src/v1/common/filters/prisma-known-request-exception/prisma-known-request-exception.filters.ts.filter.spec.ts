d07b1ac90aec3935685cfb4d9a2c1526a613dbbf
fadil-coder <fadilhambali25@gmail.com>
2025-06-25 12:26:12 +0800

    chore(common): remove unused PrismaClientKnownRequestError handler

--- src/v1/common/filters/prisma-known-request-exception/prisma-known-request-exception.filters.ts.filter.spec.ts
+++
@@ -1,7 +0,0 @@
-import { PrismaKnownRequestExceptionFilter } from './prisma-known-request-exception.filters';
-
-describe('PrismaKnownRequestExceptionFiltersTsFilter', () => {
-  it('should be defined', () => {
-    expect(new PrismaKnownRequestExceptionFilter()).toBeDefined();
-  });
-});
