6187a0ec55f055c9449289f5b1f130a1873a39f3
fadil-coder <fadilhambali25@gmail.com>
2025-06-25 12:28:19 +0800

    feat(common): update common module

--- src/v1/common/common.module.ts
+++ src/v1/common/common.module.ts
@@ -1,16 +1,11 @@
 import { Global, Module } from '@nestjs/common';
-import { PrismaKnownRequestExceptionFilter } from './filters/prisma-known-request-exception/prisma-known-request-exception.filters';
-import { APP_FILTER } from '@nestjs/core';
+
 import { ZodValidationPipe } from './pipes/zod-validation/zod-validation.pipe';
+import { PrismaWsKnownFilter } from './filters/prisma/prisma-ws-known/prisma-ws-known.filter';
+import { PrismaHttpKnownFilter } from './filters/prisma/prisma-http-known/prisma-http-known.filter';
 
 @Global()
 @Module({
-  providers: [
-    {
-      provide: APP_FILTER,
-      useClass: PrismaKnownRequestExceptionFilter,
-    },
-    ZodValidationPipe,
-  ],
+  providers: [ZodValidationPipe, PrismaWsKnownFilter, PrismaHttpKnownFilter],
 })
 export class CommonModule {}
