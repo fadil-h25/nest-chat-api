773b4e3ad3e7c7793041fff0f45fa8a7fd373ac3
fadil-coder <fadilhambali25@gmail.com>
2025-07-01 22:11:16 +0800

    feat(common): add WebSocket error filter

---
+++ src/v1/common/filters/ws/ws-custom/ws-custom.filter.spec.ts
@@ -0,0 +1,7 @@
+import { WsCustomFilter } from './ws-custom.filter';
+
+describe('WsCustomFilter', () => {
+  it('should be defined', () => {
+    expect(new WsCustomFilter()).toBeDefined();
+  });
+});
---
+++ src/v1/common/filters/ws/ws-custom/ws-custom.filter.ts
@@ -0,0 +1,42 @@
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  Inject,
+  Logger,
+} from '@nestjs/common';
+import { WsCustomException } from 'src/v1/common/exceptions/ws-custom.exception';
+import { Server, Socket } from 'socket.io';
+import { getUserIdWs } from 'src/v1/utils/auth/get-user-id.util';
+import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
+import { createLoggerMeta } from 'src/v1/utils/logger/logger.util';
+import { SocketServerHolder } from 'src/v1/common/socket/socket-server.holder';
+
+@Catch(WsCustomException)
+export class WsCustomFilter implements ExceptionFilter {
+  constructor(@Inject(WINSTON_MODULE_PROVIDER) private logger: Logger) {}
+  catch(exception: WsCustomException, host: ArgumentsHost) {
+    this.logger.debug(
+      'WsCustomFilter called',
+      createLoggerMeta('common', WsCustomException.name),
+    );
+
+    const server = SocketServerHolder.getServer();
+    const ctx = host.switchToWs();
+    const client: Socket = ctx.getClient<Socket>();
+
+    const eventName = `${exception.eventName}:error`;
+
+    const payload = {
+      statusCode: exception.statusCode,
+      message: exception.message,
+      errors: exception.errors,
+    };
+
+    const clientId = getUserIdWs(client);
+    const room: string = 'user:' + String(clientId);
+    this.logger.debug('event: ' + eventName);
+    this.logger.debug('room: ' + room);
+    server.to(room).emit(eventName, payload);
+  }
+}
