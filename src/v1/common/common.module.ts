1f11bbe37ca4106a5d53df83988c0484cba1d07d
fadil-coder <fadilhambali25@gmail.com>
2025-07-01 22:18:17 +0800

    chore(common): clean up unused imports

--- src/v1/common/common.module.ts
+++ src/v1/common/common.module.ts
@@ -3,9 +3,15 @@ import { Global, Module } from '@nestjs/common';
 import { ZodValidationPipe } from './pipes/zod-validation/zod-validation.pipe';
 import { PrismaWsKnownFilter } from './filters/prisma/prisma-ws-known/prisma-ws-known.filter';
 import { PrismaHttpKnownFilter } from './filters/prisma/prisma-http-known/prisma-http-known.filter';
+import { WsCustomFilter } from './filters/ws/ws-custom/ws-custom.filter';
 
 @Global()
 @Module({
-  providers: [ZodValidationPipe, PrismaWsKnownFilter, PrismaHttpKnownFilter],
+  providers: [
+    ZodValidationPipe,
+    PrismaWsKnownFilter,
+    PrismaHttpKnownFilter,
+    WsCustomFilter,
+  ],
 })
 export class CommonModule {}
