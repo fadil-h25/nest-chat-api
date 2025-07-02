b3cc0f1806e9a063d364646089c8b36285d5f396
fadil-coder <fadilhambali25@gmail.com>
2025-07-02 15:44:50 +0800

    feat(contact): add AddNewContactRes

--- src/v1/contact/dto/contact-response.dto.ts
+++ src/v1/contact/dto/contact-response.dto.ts
@@ -12,6 +12,34 @@ export type GetContactRes = {
   } | null;
 };
 
+export type AddNewContactRes = {
+  id: number;
+  ownerId: number;
+  name: string;
+  targetId: number;
+  totalUnreadMessage: number;
+  relationId: number | null;
+  relation: {
+    lastMessage: {
+      content: string;
+    } | null;
+  } | null;
+};
+
+export type UpdateContactRes = {
+  id: number;
+  ownerId: number;
+  name: string;
+  targetId: number;
+  totalUnreadMessage: number;
+  relationId: number | null;
+  relation: {
+    lastMessage: {
+      content: string;
+    } | null;
+  } | null;
+};
+
 export type GetContactPhoneByIdRes = {
   id: number;
   phone: string;
