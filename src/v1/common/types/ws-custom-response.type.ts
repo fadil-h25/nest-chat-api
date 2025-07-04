7117211856efc127fd36e642f3ce62b281766be6
fadil-coder <fadilhambali25@gmail.com>
2025-07-04 23:22:58 +0800

    refactor(ws): remove status property and ensure statusCode is used in WsCustomResponse type

--- src/v1/common/types/ws-custom-response.type.ts
+++ src/v1/common/types/ws-custom-response.type.ts
@@ -1,8 +1,8 @@
 import { ContactWsEvent } from 'src/v1/common/enum/contact-event';
-import { Status } from 'src/v1/common/enum/status.enum';
 
 export type WsCustomResponse<T, E extends ContactWsEvent> = {
   event: E;
-  status: Status;
+
   data: T;
+  statusCode: number;
 };
