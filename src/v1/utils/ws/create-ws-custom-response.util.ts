6e179bfc0423231d569392e847e84e32658dd908
fadil-coder <fadilhambali25@gmail.com>
2025-07-04 23:22:35 +0800

    feat(ws): update createWsCustomResponse to accept statusCode instead of fixed Status.OK

--- src/v1/utils/ws/create-ws-custom-response.util.ts
+++ src/v1/utils/ws/create-ws-custom-response.util.ts
@@ -1,14 +1,15 @@
 import { ContactWsEvent } from 'src/v1/common/enum/contact-event';
-import { Status } from 'src/v1/common/enum/status.enum';
+
 import { WsCustomResponse } from 'src/v1/common/types/ws-custom-response.type';
 
 export function createWsCustomResponse<T, E extends ContactWsEvent>(
   event: E,
   data: T,
+  statusCode: number,
 ): WsCustomResponse<T, E> {
   return {
     event,
     data,
-    status: Status.OK,
+    statusCode,
   };
 }
