5fd3ef1e533e181e6fa25cf2e336fe1c1219a0e4
fadil-coder <fadilhambali25@gmail.com>
2025-07-05 19:53:28 +0800

    refactor(contact-ws): update ContactWsEvent enum values to match event naming conventions

--- src/v1/common/enum/contact-event.ts
+++ src/v1/common/enum/contact-event.ts
@@ -1,8 +1,8 @@
 export enum ContactWsEvent {
-  CREATED_CONTACT = 'contact:created_contact',
-  CREATE_CONTACT = 'contact:create_contact',
-  UPDATE_CONTACT = 'contact:update_contact',
-  DELETE_CONTACT = 'contact:delete_contact',
-  UPDATED_CONTACT = 'contact:updated_contact',
-  DELETED_CONTACT = 'contact:deleted_contact',
+  CREATED_CONTACT = 'contact:created',
+  CREATE_CONTACT = 'contact:create',
+  UPDATE_CONTACT = 'contact:update',
+  DELETE_CONTACT = 'contact:delete',
+  UPDATED_CONTACT = 'contact:updated',
+  DELETED_CONTACT = 'contact:deleted',
 }
