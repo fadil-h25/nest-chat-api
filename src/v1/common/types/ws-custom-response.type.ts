f0e116f4745d63b44d6f60c266cb3215de845a21
fadil-coder <fadilhambali25@gmail.com>
2025-07-05 19:53:21 +0800

    refactor(ws): simplify WsCustomResponse type by removing generic event type

--- src/v1/common/types/ws-custom-response.type.ts
+++ src/v1/common/types/ws-custom-response.type.ts
@@ -1,7 +1,7 @@
 import { ContactWsEvent } from 'src/v1/common/enum/contact-event';
 
-export type WsCustomResponse<T, E extends ContactWsEvent> = {
-  event: E;
+export type WsCustomResponse<T> = {
+  event: ContactWsEvent;
 
   data: T;
   statusCode: number;
