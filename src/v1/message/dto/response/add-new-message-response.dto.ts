c3eb1ea89d2301b5a1aeef062f17276c61e891f4
fadil-coder <fadilhambali25@gmail.com>
2025-07-07 01:18:54 +0800

    feat(message): add respon type for UpdateMessage and DeleteMessage

--- src/v1/message/dto/response/add-new-message-response.dto.ts
+++ src/v1/message/dto/response/add-new-message-response.dto.ts
@@ -6,3 +6,16 @@ export type AddNewMessageResponseDto = {
   createdAt: Date;
   updatedAt: Date;
 };
+
+export type UpdateMessageResponseDto = {
+  id: number;
+  content: string;
+  relationId: number;
+  ownerId: number;
+  createdAt: Date;
+  updatedAt: Date;
+};
+
+export type DeleteMessageResponseDto = {
+  id: number;
+};
