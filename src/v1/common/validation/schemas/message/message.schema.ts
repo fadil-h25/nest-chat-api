4302ee562d81d6479ef2171653a0a89396a9bf80
fadil-coder <fadilhambali25@gmail.com>
2025-07-08 13:28:43 +0800

    feat(common): add message prefix to each schema name

--- src/v1/common/validation/schemas/message/message.schema.ts
+++ src/v1/common/validation/schemas/message/message.schema.ts
@@ -1,10 +1,10 @@
 import z from 'zod';
 
 export const messageIdSchema = z.number().int().positive();
-export const ownerIdSchema = z.number().int().positive();
-export const contentSchema = z.string().min(1);
-export const relationIdSchema = z.number().int().positive();
-export const isReadSchema = z.boolean().optional();
+export const messageOwnerIdSchema = z.number().int().positive();
+export const messageContentSchema = z.string().min(1);
+export const messageRelationIdSchema = z.number().int().positive();
+export const messageIsReadSchema = z.boolean().optional();
 
-export const createdAtSchema = z.date();
-export const updatedAtSchema = z.date();
+export const messageCreatedAtSchema = z.date();
+export const messageUpdatedAtSchema = z.date();
