e05a10b1083f83eaccd944025d791396bdbcb13f
fadil-coder <fadilhambali25@gmail.com>
2025-07-13 00:40:48 +0800

    feat(common): add enums RoomScope and UserEvent

---
+++ src/v1/common/enum/room-scope.enum.ts
@@ -0,0 +1,4 @@
+export enum RoomScope {
+  USER_ROOM_PREFIX = 'user-room:',
+  MESSAGE_ROOM_PREFIX = 'message-room:',
+}
---
+++ src/v1/common/enum/user-event.enum.ts
@@ -0,0 +1,5 @@
+export enum UserEvent {
+  USER_JOIN_ROOM = 'user-room:join',
+  USER_LEFT_ROOM = 'user-room:left',
+  USER_ROOM_PREFIX = 'user-room:', // atau USER_NOTIFICATION_ROOM
+}
