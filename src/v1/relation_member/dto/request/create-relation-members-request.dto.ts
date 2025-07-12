bf7135a935f50b60ee7ba63dbe0d538f0ebb94b1
fadil-coder <fadilhambali25@gmail.com>
2025-07-13 00:30:41 +0800

    feat(relation-member): add createRelationMember method with dto

---
+++ src/v1/relation_member/dto/request/create-relation-members-request.dto.ts
@@ -0,0 +1,4 @@
+export type CreateRelationMembersRequestDto = {
+  relationId: number;
+  userId: number;
+};
--- src/v1/relation_member/relation_member.service.ts
+++ src/v1/relation_member/relation_member.service.ts
@@ -3,10 +3,22 @@ import { DatabaseService } from '../../database/database.service';
 import { RelationMember, Prisma } from '@prisma/client';
 import { GetRelationIdsWithBothUsersRes } from './dto/relation-member-response.dto';
 import { AddNewRelationMemberDto } from '../common/validation/schemas/relation-member.schema';
+import { CreateRelationMembersRequestDto } from './dto/request/create-relation-members-request.dto';
 
 @Injectable()
 export class RelationMemberService {
   constructor(private databaseService: DatabaseService) {}
+
+  async createRelationMembers(
+    data: CreateRelationMembersRequestDto[],
+    tx?: Prisma.TransactionClient,
+  ) {
+    const db = tx ?? this.databaseService;
+
+    await db.relationMember.createMany({
+      data,
+    });
+  }
 
   async addNewRelationMember(
     userId: number,
