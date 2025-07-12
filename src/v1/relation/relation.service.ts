6aae527ade80cb3f5b9601e09b7f0259e43f4733
fadil-coder <fadilhambali25@gmail.com>
2025-07-13 00:32:28 +0800

    feat(relation): rename addNewRelation to createRelation and remove internal validation

--- src/v1/relation/relation.service.ts
+++ src/v1/relation/relation.service.ts
@@ -1,19 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { DatabaseService } from '../../database/database.service';
 import { Relation, RelationType, Prisma } from '@prisma/client';
-import { validationRelationType } from '../common/validation/schemas/relation/relation.schema';
 
 @Injectable()
 export class RelationService {
   constructor(private readonly databaseService: DatabaseService) {}
 
-  async addNewRelation(
+  async createRelation(
     relationType: RelationType,
     tx?: Prisma.TransactionClient,
   ): Promise<Relation> {
     const db = tx ?? this.databaseService;
-
-    relationType = validationRelationType.parse(relationType);
 
     const relation = await db.relation.create({
       data: {
