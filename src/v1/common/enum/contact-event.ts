b5789b9cbd4564b3a75ca15fdc34930dc9456f53
fadil-coder <fadilhambali25@gmail.com>
2025-07-13 00:42:15 +0800

    refactor(common): rename some event name at in ContactEvent

--- src/v1/common/enum/contact-event.ts
+++ src/v1/common/enum/contact-event.ts
@@ -1,8 +1,11 @@
 export enum ContactWsEvent {
-  CREATED_CONTACT = 'contact:created',
+  // Client → Server
   CREATE_CONTACT = 'contact:create',
   UPDATE_CONTACT = 'contact:update',
   DELETE_CONTACT = 'contact:delete',
-  UPDATED_CONTACT = 'contact:updated',
-  DELETED_CONTACT = 'contact:deleted',
+
+  // Server → Client
+  CONTACT_CREATED = 'contact:created',
+  CONTACT_UPDATED = 'contact:updated',
+  CONTACT_DELETED = 'contact:deleted',
 }
