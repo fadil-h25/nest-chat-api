8060b6aa8578a8c386f96187f3a5585bba54e6d7
fadil-coder <fadilhambali25@gmail.com>
2025-07-13 01:11:45 +0800

    refactor(relation-member): remove unused method from service

--- src/v1/relation_member/relation_member.service.ts
+++ src/v1/relation_member/relation_member.service.ts
@@ -1,8 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { DatabaseService } from '../../database/database.service';
-import { RelationMember, Prisma } from '@prisma/client';
-import { GetRelationIdsWithBothUsersRes } from './dto/relation-member-response.dto';
-import { AddNewRelationMemberDto } from '../common/validation/schemas/relation-member.schema';
+import { Prisma } from '@prisma/client';
+
 import { CreateRelationMembersRequestDto } from './dto/request/create-relation-members-request.dto';
 
 @Injectable()
@@ -18,59 +17,5 @@ export class RelationMemberService {
     await db.relationMember.createMany({
       data,
     });
-  }
-
-  async addNewRelationMember(
-    userId: number,
-    relationId: number,
-    tx?: Prisma.TransactionClient,
-  ): Promise<RelationMember> {
-    const db = tx ?? this.databaseService;
-
-    const relationMember = await db.relationMember.create({
-      data: {
-        relationId,
-        userId,
-      },
-    });
-
-    return relationMember;
-  }
-
-  async addTwoNewRelationMembers(
-    data: AddNewRelationMemberDto[],
-    tx?: Prisma.TransactionClient,
-  ): Promise<void> {
-    const db = tx ?? this.databaseService;
-
-    await db.relationMember.createMany({
-      data,
-    });
-  }
-
-  async getRelationIdsWithBothUsers(
-    userId1: number,
-    userId2: number,
-  ): Promise<GetRelationIdsWithBothUsersRes> {
-    const relationMembers = await this.databaseService.relationMember.groupBy({
-      by: 'relationId',
-      where: {
-        userId: {
-          in: [userId1, userId2],
-        },
-      },
-      having: {
-        userId: {
-          _count: {
-            equals: 2,
-          },
-        },
-      },
-      _count: {
-        userId: true,
-      },
-    });
-
-    return relationMembers;
   }
 }
