b5f6df77e79a39a9bdbb1775baab66017ab78453
fadil-coder <fadilhambali25@gmail.com>
2025-07-21 00:01:46 +0800

    feat(common): support more event types in generic param E

--- src/v1/common/types/ws-custom-response.type.ts
+++ src/v1/common/types/ws-custom-response.type.ts
@@ -1,7 +1,11 @@
 import { ContactWsEvent } from 'src/v1/common/enum/contact-event';
 import { MessageWsEvent } from '../enum/message-event';
+import { RelationMemberWsEvent } from '../enum/relation-member-event';
 
-export type WsCustomResponse<E extends ContactWsEvent | MessageWsEvent, T> = {
+export type WsCustomResponse<
+  E extends ContactWsEvent | MessageWsEvent | RelationMemberWsEvent,
+  T,
+> = {
   event: E;
 
   data: T;
