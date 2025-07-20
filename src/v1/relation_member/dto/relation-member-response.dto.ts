e3a2e7641bbcd37883f51d59655e926ba683915c
fadil-coder <fadilhambali25@gmail.com>
2025-07-21 00:04:35 +0800

    feat(relation-member): add response type for FindRelationMemberResponse method

--- src/v1/relation_member/dto/relation-member-response.dto.ts
+++ src/v1/relation_member/dto/relation-member-response.dto.ts
@@ -1,6 +1,20 @@
+import { RelationType } from '@prisma/client';
+
 export type GetRelationIdsWithBothUsersRes = {
   relationId: number;
   _count: {
     userId: number;
   };
 }[];
+
+export type FindRelationMemberResponse = {
+  totalUnreadMessage: number;
+  relation: {
+    id: number;
+    type: RelationType;
+    lastMessageId: number | null;
+  };
+  id: number;
+  relationId: number;
+  userId: number;
+};
