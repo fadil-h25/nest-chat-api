380c7ed1c49499f9eb6d0b202304c73d097bb37e
fadil-coder <fadilhambali25@gmail.com>
2025-07-21 00:00:01 +0800

    chore(utils): rename parameter field id to relationId

--- src/v1/utils/ws/generate-room-name.util.ts
+++ src/v1/utils/ws/generate-room-name.util.ts
@@ -1,5 +1,5 @@
 import { RoomScope } from 'src/v1/common/enum/room-scope.enum';
 
-export function generateRoomName(scope: RoomScope, id: number): string {
-  return `${scope}:${id}`;
+export function generateRoomName(scope: RoomScope, relationId: number): string {
+  return `${scope}:${relationId}`;
 }
