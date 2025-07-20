6b656a307de742018705f1b0d941030b72248587
fadil-coder <fadilhambali25@gmail.com>
2025-07-21 00:06:30 +0800

    feat(relation-member): add findRelationMembers method

--- src/v1/relation_member/relation_member.module.ts
+++ src/v1/relation_member/relation_member.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { RelationMemberService } from './relation_member.service';
 import { DatabaseModule } from 'src/database/database.module';
+import { MessageModule } from '../message/message.module';
 
 @Module({
   providers: [RelationMemberService],
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, MessageModule],
   exports: [RelationMemberService],
 })
 export class RelationMemberModule {}
--- src/v1/relation_member/relation_member.service.ts
+++ src/v1/relation_member/relation_member.service.ts
@@ -3,10 +3,17 @@ import { DatabaseService } from '../../database/database.service';
 import { Prisma } from '@prisma/client';
 
 import { CreateRelationMembersRequestDto } from './dto/request/create-relation-members-request.dto';
+import { FindRelationMembersRequestDto } from './dto/request/find-relation-members-request.dto';
+import { MessageService } from '../message/message.service';
+
+import { RelationMemberResponse } from './dto/response/relation-member-response.dto';
 
 @Injectable()
 export class RelationMemberService {
-  constructor(private databaseService: DatabaseService) {}
+  constructor(
+    private databaseService: DatabaseService,
+    private messageService: MessageService,
+  ) {}
 
   async createRelationMembers(
     data: CreateRelationMembersRequestDto[],
@@ -17,5 +24,58 @@ export class RelationMemberService {
     await db.relationMember.createMany({
       data,
     });
+  }
+
+  async findRelationMembers(
+    data: FindRelationMembersRequestDto,
+  ): Promise<RelationMemberResponse[]> {
+    const relationMembersData =
+      await this.databaseService.relationMember.findMany({
+        where: {
+          relationId: data.relationId, //mengambil lawan bicara ketika
+          NOT: {
+            userId: data.userId,
+          },
+        },
+        select: {
+          relation: {
+            select: {
+              id: true,
+              type: true,
+              lastMessage: {
+                select: {
+                  id: true,
+                  content: true,
+                  updatedAt: true,
+                  createdAt: true,
+                },
+              },
+            },
+          },
+          user: {
+            select: {
+              id: true,
+              phone: true,
+            },
+          },
+        },
+      });
+    const relationMembersDataWithUnreadCounts = await Promise.all(
+      relationMembersData.map(async (dataRelation) => {
+        const totalUnreadMessage =
+          await this.messageService.countTotalUnreadMessage(
+            data.userId,
+            data.relationId,
+          );
+
+        return {
+          ...dataRelation,
+          totalUnreadMessage,
+          ownerId: data.userId,
+        };
+      }),
+    );
+
+    return relationMembersDataWithUnreadCounts;
   }
 }
