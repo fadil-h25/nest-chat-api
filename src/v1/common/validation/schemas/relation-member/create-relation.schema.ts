c91fececf1e1d109560508a5a4fbc1b850225345
fadil-coder <fadilhambali25@gmail.com>
2025-07-21 00:13:26 +0800

    feat(common): remove unused field

--- src/v1/common/validation/schemas/relation-member/create-relation.schema.ts
+++ src/v1/common/validation/schemas/relation-member/create-relation.schema.ts
@@ -1,10 +1,8 @@
 import z from 'zod';
 import { relationIdSchema } from '../relation/relation.schema';
 import { userIdSchema } from '../user/user.schema';
-import { relationMemberIdSchema } from './relation-member.schema';
 
 export const CreateRelationMemberSchema = z.object({
-  id: relationMemberIdSchema,
   userId: userIdSchema,
   relationId: relationIdSchema,
 });
