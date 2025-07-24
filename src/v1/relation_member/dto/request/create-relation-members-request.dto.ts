9793fda7d407a2c0ed7925d8e907a628bcd7c946
fadil-coder <fadilhambali25@gmail.com>
2025-07-25 00:30:20 +0800

    refactor(relation-member): remove unused fto

--- src/v1/relation_member/dto/request/create-relation-members-request.dto.ts
+++
@@ -1,4 +0,0 @@
-export type CreateRelationMembersRequestDto = {
-  relationId: number;
-  userId: number;
-};
--- src/v1/relation_member/dto/request/find-relation-members-request.dto.ts
+++
@@ -1,6 +0,0 @@
-import { FindRelationMembersSchema } from 'src/v1/common/validation/schemas/relation-member/find-relation-member.schema';
-import { z } from 'zod';
-
-export type FindRelationMembersRequestDto = z.infer<
-  typeof FindRelationMembersSchema
->;
