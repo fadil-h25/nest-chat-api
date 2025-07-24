b5fe8274119e39f08f73e765f2eef870595d17e7
fadil-coder <fadilhambali25@gmail.com>
2025-07-25 02:06:58 +0800

    feat(common): modify schema by adding and removing fields

--- src/v1/common/validation/schemas/message/create-message.schema.ts
+++ src/v1/common/validation/schemas/message/create-message.schema.ts
@@ -1,16 +1,11 @@
 import z from 'zod';
 import { messageContentSchema, messageIsReadSchema } from './message.schema';
 import { userIdSchema } from '../user/user.schema';
-import {
-  relationIdSchemaNullable,
-  relationTypeSchema,
-} from '../relation/relation.schema';
+import { relationTypeSchema } from '../relation/relation.schema';
 
 export const createMessageSchema = z.object({
-  ownerId: userIdSchema,
   content: messageContentSchema,
-  relationId: relationIdSchemaNullable,
-  relationType: relationTypeSchema,
   isRead: messageIsReadSchema,
   targetId: userIdSchema,
+  relationType: relationTypeSchema,
 });
