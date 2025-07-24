c9ee2a192cef869f9c0f46479891f5f5ea53a29e
fadil-coder <fadilhambali25@gmail.com>
2025-07-25 02:11:06 +0800

    feat(common): modify updateMessageSchema schema by adding and removing fields

--- src/v1/common/validation/schemas/message/update-message.schema.ts
+++ src/v1/common/validation/schemas/message/update-message.schema.ts
@@ -2,15 +2,11 @@ import { z } from 'zod';
 import {
   messageIdSchema,
   messageContentSchema,
-  messageOwnerIdSchema,
   messageRelationIdSchema,
-  messageIsReadSchema,
 } from './message.schema';
 
 export const updateMessageSchema = z.object({
   id: messageIdSchema,
-  content: messageContentSchema,
-  ownerId: messageOwnerIdSchema,
   relationId: messageRelationIdSchema,
-  isRead: messageIsReadSchema,
+  content: messageContentSchema,
 });
