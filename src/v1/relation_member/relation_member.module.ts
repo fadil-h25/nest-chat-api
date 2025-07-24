7aee3f684da342af2e26483f7b4421be3d274d3c
fadil-coder <fadilhambali25@gmail.com>
2025-07-25 00:42:56 +0800

    feat(relation-member): fix import

--- src/v1/relation_member/relation_member.module.ts
+++ src/v1/relation_member/relation_member.module.ts
@@ -1,11 +1,13 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { RelationMemberService } from './relation_member.service';
 import { DatabaseModule } from 'src/database/database.module';
 import { MessageModule } from '../message/message.module';
+import { RelationMemberController } from './relation_member.controller';
 
 @Module({
   providers: [RelationMemberService],
-  imports: [DatabaseModule, MessageModule],
+  imports: [DatabaseModule, forwardRef(() => MessageModule)],
   exports: [RelationMemberService],
+  controllers: [RelationMemberController],
 })
 export class RelationMemberModule {}
