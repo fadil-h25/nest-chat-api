c1bf4a03197a68d69fb5f6248429365c79d63b52
fadil-coder <fadilhambali25@gmail.com>
2025-07-25 02:11:43 +0800

    feat(common): add schema validation for findRelationMember method

--- src/v1/common/validation/schemas/relation-member/find-relation-member.schema.ts
+++ src/v1/common/validation/schemas/relation-member/find-relation-member.schema.ts
@@ -1,8 +1,14 @@
 import { z } from 'zod';
 import { relationIdSchema } from '../relation/relation.schema';
 import { userIdSchema } from '../user/user.schema';
+import { relationMemberIdSchema } from './relation-member.schema';
 
 export const FindRelationMembersSchema = z.object({
   relationId: relationIdSchema,
   userId: userIdSchema,
+});
+
+export const FindRelationMemberByUserAndTargetSchema = z.object({
+  userId: userIdSchema,
+  targetId: userIdSchema,
 });
