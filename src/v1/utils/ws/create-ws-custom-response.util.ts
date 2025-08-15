dfc8209f6b28d896ea8d2637747cd3e00554b435
fadil-coder <fadilhambali25@gmail.com>
2025-08-15 22:56:23 +0800

    chore(utils): comment out unused imports and function definition in create-ws-custom-response utility

--- src/v1/utils/ws/create-ws-custom-response.util.ts
+++ src/v1/utils/ws/create-ws-custom-response.util.ts
@@ -1,15 +1,15 @@
-import { ContactWsEvent } from 'src/v1/common/enum/contact-event';
-import { MessageWsEvent } from 'src/v1/common/enum/message-event';
-import { RelationMemberWsEvent } from 'src/v1/common/enum/relation-member-event';
-import { WsCustomResponse } from 'src/v1/common/types/ws-custom-response.type';
+// import { ContactWsEvent } from 'src/v1/common/enum/contact-event';
+// import { MessageWsEvent } from 'src/v1/common/enum/message-event';
+// import { RelationMemberWsEvent } from 'src/v1/common/enum/relation-member-event';
+// // import { WsCustomResponse } from 'src/v1/common/types/ws-custom-response.type';
 
-export function createWsCustomResponse<
-  E extends ContactWsEvent | MessageWsEvent | RelationMemberWsEvent,
-  T,
->(event: E, data: T, statusCode: number): WsCustomResponse<E, T> {
-  return {
-    event,
-    data,
-    statusCode,
-  };
-}
+// export function createWsCustomResponse<
+//   E extends ContactWsEvent | MessageWsEvent | RelationMemberWsEvent,
+//   T,
+// >(event: E, data: T, statusCode: number): WsCustomResponse<E, T> {
+//   return {
+//     event,
+//     data,
+//     statusCode,
+//   };
+// }
