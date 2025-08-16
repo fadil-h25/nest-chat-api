cb859408becef2c53d34b90065fef2ddec1b8072
fadil-coder <fadilhambali25@gmail.com>
2025-08-17 00:07:18 +0800

    refactor(generate-ws-response): comment out unused import and function

--- src/v1/common/utils/generate-ws-response.ts
+++ src/v1/common/utils/generate-ws-response.ts
@@ -1,9 +1,11 @@
-import { WsCustomResponseAck } from '../types/ws-custom-response.type';
+// import { WsCustomResponseAck } from '../types/ws-custom-response.type';
 
-export function generateResponseAck(event: string): WsCustomResponseAck {
-  return {
-    statusCode: 200,
-    event,
-    data: {},
-  };
-}
+// export function generateResponseAck(event: string): WsCustomResponseAck {
+//   return {
+//     statusCode: 200,
+//     event,
+//     data: {
+//       test: 'halo',
+//     },
+//   };
+// }
