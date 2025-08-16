363977f28f19c3a063a3f245ba5f33cea369fb47
fadil-coder <fadilhambali25@gmail.com>
2025-08-17 00:10:02 +0800

    feat(common): add RelationWsEvent enum for WebSocket relation events

---
+++ src/v1/common/enum/relation-event.ts
@@ -0,0 +1,5 @@
+export enum RelationWsEvent {
+  JOIN_RELATION_ROOM = 'relation-room:join',
+  LEFT_RELATION_ROOM = 'relation-room:left',
+  RELATION_ROOM_PREFIX = 'relation-room:',
+}
--- src/v1/common/enum/user-event.enum.ts
+++ src/v1/common/enum/user-event.enum.ts
@@ -1,4 +1,4 @@
-export enum UserEvent {
+export enum UserWsEvent {
   USER_JOIN_ROOM = 'user-room:join',
   USER_LEFT_ROOM = 'user-room:left',
   USER_ROOM_PREFIX = 'user-room:', // atau USER_NOTIFICATION_ROOM
