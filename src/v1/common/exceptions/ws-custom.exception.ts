a60930992ff8977e55837d8e0823a89cc90c0abb
fadil-coder <fadilhambali25@gmail.com>
2025-08-17 00:09:41 +0800

    feat(common): extend WsCustomException to inherit from WsException for improved error handling

--- src/v1/common/exceptions/ws-custom.exception.ts
+++ src/v1/common/exceptions/ws-custom.exception.ts
@@ -1,12 +1,13 @@
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 import { ZodError } from 'zod';
+import { WsException } from '@nestjs/websockets';
 
 import { ErrorCustom } from '../errors/error-custom.type';
 import { prismaErrorMapper } from '../errors/mappers/prisma-error.mapper';
 import { zodErrorMapper } from '../errors/mappers/zod-error.mapper';
 import { Status } from '../enum/status.enum';
 
-export class WsCustomException extends Error {
+export class WsCustomException extends WsException {
   public readonly eventName: string;
   public readonly statusCode: number;
   public readonly errors: ErrorCustom[];
@@ -14,30 +15,37 @@ export class WsCustomException extends Error {
   public readonly status: Status = Status.ERROR;
 
   constructor(eventName: string, message: string, originalError: unknown) {
-    super(message);
-    this.eventName = eventName;
-    this.originalError = originalError;
-    this.errors = [];
-    this.statusCode = 500;
+    const defaultStatus = 500;
+    const errors: ErrorCustom[] = [];
+    let statusCode = defaultStatus;
 
     if (originalError instanceof ErrorCustom) {
-      this.errors.push(originalError);
-      this.statusCode = originalError.statusCode;
+      errors.push(originalError);
+      statusCode = originalError.statusCode;
     } else if (originalError instanceof PrismaClientKnownRequestError) {
       const error = prismaErrorMapper(originalError);
-      this.statusCode = error.statusCode;
-      this.errors.push(error);
+      statusCode = error.statusCode;
+      errors.push(error);
     } else if (originalError instanceof ZodError) {
-      this.errors = zodErrorMapper(originalError);
-      this.statusCode = 400;
+      errors.push(...zodErrorMapper(originalError));
+      statusCode = 400;
     } else if (originalError instanceof Error) {
-      // Optional: jika error biasa tapi bukan ErrorCustom
-      this.errors.push(
-        new ErrorCustom(this.statusCode, '', originalError.message),
-      );
+      errors.push(new ErrorCustom(defaultStatus, '', originalError.message));
     } else {
-      // Optional: jika originalError bukan objek error
-      this.errors.push(new ErrorCustom(this.statusCode, '', 'Unknown error'));
+      errors.push(new ErrorCustom(defaultStatus, '', 'Unknown error'));
     }
+
+    super({
+      status: Status.ERROR,
+      eventName,
+      statusCode,
+      errors,
+      message,
+    });
+
+    this.eventName = eventName;
+    this.originalError = originalError;
+    this.statusCode = statusCode;
+    this.errors = errors;
   }
 }
